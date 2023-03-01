e394dbd4f0eb98c5ac1af8dae444f8d08282c414
Dario <dariomadau@hotmail.com>
2023-03-01 15:53:42 +0100

    added responsive grid

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const container = document.getElementById("container");
+const slider = document.getElementById("myRange");
+const sliderText = document.getElementById("sliderValue");
+let gridValue = `1fr `;
 
-// create 16 column containers
-for (i = 0; i < 16; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("column");
-  container.appendChild(newDiv);
-  // for each column create 16 divs
-  for (x = 0; x < 16; x++) {
-    const pixelDiv = document.createElement("div");
-    pixelDiv.classList.add("pixel");
-    newDiv.appendChild(pixelDiv);
+function setGridColumns() {
+  container.style.gridTemplateColumns = `${gridValue.repeat(slider.value)}`;
+}
+setGridColumns();
+
+// create x column containers
+function createPixels() {
+  for (i = 0; i < slider.value; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("column");
+    container.appendChild(newDiv);
+    // for each column create x divs
+    for (x = 0; x < slider.value; x++) {
+      const pixelDiv = document.createElement("div");
+      pixelDiv.classList.add("pixel");
+      newDiv.appendChild(pixelDiv);
+    }
   }
 }
+createPixels();
 
-//create nodelist of all pixels
-const pixelList = document.querySelectorAll(".pixel");
-//turn that nodelist into an array
-const pixelArray = [...pixelList];
-
-//check each pixel for event
-pixelArray.forEach((pixel) => {
-  pixel.addEventListener("mouseover", () => {
-    pixel.classList.add("activepixel");
+function setPixelActive() {
+  //create nodelist of all pixels
+  const pixelList = document.querySelectorAll(".pixel");
+  //turn that nodelist into an array
+  const pixelArray = [...pixelList];
+  //check each pixel for event
+  pixelArray.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.add("activepixel");
+    });
   });
-});
+}
+setPixelActive();
 
-// for i create x containers that stack vertically
-// for i create x amount of divs in container that stack horizzontally
-// get all divs, add event listener, on hover change background color
+//slider logic
+sliderText.innerText = `${slider.value} x ${slider.value} `;
+slider.oninput = () => {
+  setGridColumns();
+  sliderText.innerText = `${slider.value} x ${slider.value} `;
+  container.innerHTML = "";
+  createPixels();
+  setPixelActive();
+};
